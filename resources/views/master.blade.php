f05688a5fa34fc9a4acbc0743189886cd32df61f
Jaras <jaroslav.jankelevic@gmail.com>
2017-06-18 22:38:23 +0300

    Papildymas

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,9 +8,9 @@
 
 	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
 
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.css">
+	<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.css"> -->
 
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.print.css">
+	<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.print.css"> -->
 
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 
@@ -18,20 +18,18 @@
 
 	<link rel="stylesheet" type="text/css" href="{{ URL::asset('css/app.css') }}">
 
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
 </head>
 <body>
 	
 
 	@yield('content')
-
-
-
-  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
 
 
 
--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -4,7 +4,7 @@
 <div class="container text-center col-md-12 main-container">
 
 
-	<div class="nav">
+	<div class="custom-nav">
 
     <div class="nav-header">
       <h4><strong>Hello username!</strong></h4>
@@ -16,14 +16,21 @@
 
 <div class="team">
 
-  <h5 class="booking-title">Choose</h5>
+  <h5 class="booking-title">Pasirinkite meistrą</h5>
+
+  <select>
+    <option value="redas">Redas</option>
+    <option value="zygmas">Zygmas</option>
+    <option value="birutė">Birutė</option>
+    <option value="pranas">Pranas</option>
+  </select>
 
 
 
 </div>
 
 <div class="services">
-  <a href="/admin/validate"><h5 class="booking-title">Choose Services</h5></a>
+  <a href="/admin/validate"><h5 class="booking-title">Pasirinkite procedūras</h5></a>
 
   <form>
     <label class="checkbox-inline">
@@ -35,16 +42,18 @@
     <label class="checkbox-inline">
       Šventinis Šukavimas <input type="checkbox" value="">
     </label>
-    <label class="checkbox-inline">
-      Barzdos kirpimas<input type="checkbox" value="">
-    </label>
-    <label class="checkbox-inline">
-      Manikiūras<input type="checkbox" value="">
-    </label>
-    <label class="checkbox-inline">
-      Barzdos kirpimas<input type="checkbox" value="">
-    </label>
+  <!-- <label class="checkbox-inline"> -->
+      <!-- Barzdos kirpimas<input type="checkbox" value=""> -->
+    <!-- </label> -->
+    <!-- <label class="checkbox-inline"> -->
+      <!-- Manikiūras<input type="checkbox" value=""> -->
+    <!-- </label> -->
+    <!-- <label class="checkbox-inline"> -->
+      <!-- Barzdos kirpimas<input type="checkbox" value=""> -->
+    <!-- </label> -->
   </form>
+
+</div> <!--end services-->
 
 
 
@@ -65,22 +74,111 @@
 
 
 
-<div class="main">
+<div class="row main">
+
+  <div class="upcoming-bookings bookings">
+    <h5>Ateinantis užsakymai</h5>
+    <table class = "table table-striped">
+  
+   
+   <thead>
+      <tr>
+         <th>Data ir laikas</th>
+         <th>Meistras</th>
+         <th>Procedūros</th>
+         <th>Kaina</th>
+      </tr>
+   </thead>
+   
+   <tbody>
+      <tr>
+         <td>2017.06.29 19:00</td>
+         <td>Redas</td>
+         <td>Plaukų kirpimas; Barzdos kirpimas</td>
+         <td>30 &euro;</td>
+      </tr>
+      
+      <tr>
+         <td>2017.07.05 19:00</td>
+         <td>Redas</td>
+         <td>Plaukų kirpimas; Barzdos kirpimas</td>
+         <td>30 &euro;</td>
+      </tr>
+      
+      <tr>
+         <td>2017.07.29 18:15</td>
+         <td>Redas</td>
+         <td>Plaukų kirpimas; Barzdos kirpimas</td>
+         <td>30 &euro;</td>
+      </tr>
+   </tbody>
+   
+</table>
+  </div>
+
+  <div class="previous-bookings bookings">
+    <h5>Buvę užsakymai</h5>
+
+        <table class = "table table-striped">  
+   
+   <thead>
+      <tr>
+         <th>Data ir laikas</th>
+         <th>Meistras</th>
+         <th>Procedūros</th>
+         <th>Kaina</th>
+      </tr>
+   </thead>
+   
+   <tbody>
+      <tr>
+         <td>2017.05.29 19:00</td>
+         <td>Redas</td>
+         <td>Plaukų kirpimas; Barzdos kirpimas</td>
+         <td>30 &euro;</td>
+      </tr>
+      
+      <tr>
+         <td>2017.04.05 19:00</td>
+         <td>Redas</td>
+         <td>Plaukų kirpimas; Barzdos kirpimas</td>
+         <td>30 &euro;</td>
+      </tr>
+      
+      <tr>
+         <td>2017.03.29 18:15</td>
+         <td>Redas</td>
+         <td>Plaukų kirpimas; Barzdos kirpimas</td>
+         <td>30 &euro;</td>
+      </tr>
+   </tbody>
+   
+</table>
+  </div>
 
 
-</div>
+</div><!--end main-->
 
 
-</div><!--end of .container-->
+
+
+
+  </div><!--end of .container-->
+
 
   <footer class="footer">
     <p class="text-center">All rights reserved &copy 2017</p>
   </footer>
 
   <script>
+
+  $(document).ready(function () {
+
   $( function() {
     $( "#datepicker" ).datepicker();
   } );
+
+  });
 
   </script>
 
